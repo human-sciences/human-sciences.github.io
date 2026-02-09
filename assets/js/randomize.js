43faeee47b66d66cf3c78570fb7a70d9e5dc6555
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-02-09 01:23:34 -0800

    tidy upjs

--- assets/js/randomize.js
+++ assets/js/randomize.js
@@ -1,11 +1,9 @@
+// Randomize the order of the people on the people page!
 function randomize_order(){
-	console.log('randomizing')
 	let parent = document.querySelector('.post-content');
 	let people = document.querySelectorAll('.post-content > p');
-	console.log(people);
 	let shuffled = Array.from(people).sort(() => Math.random() - 0.5);
 	shuffled.forEach(person => parent.appendChild(person));
-	console.log(shuffled);
 }
 
 document.addEventListener("load", randomize_order());
